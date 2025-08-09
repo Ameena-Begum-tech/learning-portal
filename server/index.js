a3e0620e746d5862e554f38a34128f4732c2714e
Ameena Begum <ameenabegum140@example.com>
2025-08-09 15:54:02 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from  'dotenv';
+import { connectDB } from './database/db.js';
+
+dotenv.config();
+
+const app = express();
+const port=process.env.PORT ;
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
+   connectDB();
+});
